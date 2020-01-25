a43174d6db7b51c2de516fcc045ac52b2f85cb14
Katie Hinkle <katie@Katies-MacBook-Air.local>
2020-01-24 18:17:06 -0800

    To fix the Nomethod error I went into the views folder and added .id to school. I also went into the schools controller and changed the def index instead of School.first. I put School.all. I can now offically boot the app without running an error. Now when I go to add a new school I get a template error.

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -3,7 +3,7 @@ class SchoolsController < ApplicationController
   #before_action :load_school, only: %i(show edit update destroy)
 
   def index
-    @schools = School.first
+    @schools = School.all
   end
 
   def show
